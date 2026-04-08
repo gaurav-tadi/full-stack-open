ad097245b81cbf872c073034dc2e0130bfaa537b
gaurav tadi <gauravtadi11@gmail.com>
2026-04-08 12:31:00 +0530

    done all part 4 exercises

--- part4/controllers/blogs.js
+++ part4/controllers/blogs.js
@@ -1,19 +1,72 @@
-const blogRouter = require("express").Router()
+const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
+const { userExtractor } = require('../utils/middleware')
 
-blogRouter.get('/', (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs)
-  })
+blogsRouter.get('/', async (request, response) => {
+  const blogs = await Blog
+    .find({})
+    .populate('user', { username: 1, name: 1 })
+  response.json(blogs)
 })
 
-blogRouter.post('/', (request, response) => {
-  const blog = new Blog(request.body)
+blogsRouter.post('/', userExtractor, async (request, response) => {
+  const { user } = request
 
-  blog.save().then((result) => {
-    response.status(201).json(result)
+  if (!user) {
+    return response.status(401).json({ error: 'token invalid' })
+  }
+
+  const { title, author, url, likes } = request.body
+
+  if (!title || !url) {
+    return response.status(400).json({ error: 'title or url is missing' })
+  }
+
+  const blog = new Blog({
+    title,
+    author,
+    url,
+    likes: likes ?? 0,
+    user: user
   })
+
+  const savedBlog = await blog.save()
+
+  user.blogs = user.blogs.concat(savedBlog._id)
+  await user.save({ validateModifiedOnly: true })
+
+  response.status(201).json(savedBlog)
 })
 
+blogsRouter.put('/:id', async (request, response) => {
+  const { title, author, url, likes } = request.body
 
-module.exports = blogRouter
+  const updatedBlog = await Blog.findByIdAndUpdate(
+    request.params.id,
+    { title, author, url, likes },
+    { new: true }
+  ).populate('user', { username: 1, name: 1 })
+
+  response.json(updatedBlog)
+})
+
+blogsRouter.delete('/:id', userExtractor, async (request, response) => {
+  const { user } = request
+
+  if (!user) {
+    return response.status(401).json({ error: 'token invalid' })
+  }
+
+  const blog = await Blog.findById(request.params.id)
+
+  if ( blog?.user.toString() === user._id.toString() ){
+    await Blog.findByIdAndRemove(request.params.id)
+    response.status(204).end()
+  }
+  else{
+    response.status(401).json({ error: 'unauthorized access' })
+  }
+
+})
+
+module.exports = blogsRouter
